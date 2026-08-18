0ed57c353b1222901af82d7169adf30f2baf2cab
giomaldonado <giomaldonado@proton.me>
2026-08-18 11:27:26 -0600

    finished foundations 1- 4

--- foundations/03_numberChecker/numberChecker.js
+++ foundations/03_numberChecker/numberChecker.js
@@ -1,9 +1,11 @@
 function numberChecker(number) {
-  if (number === 6) {
-    return true;
-  } else {
+  if (number === 6 || number ===9) {
     return false;
+  } else if (number === 10 || number === 1000) {
+    
+    return true;
   }
+  
 }
 
 // Do not edit below this line
