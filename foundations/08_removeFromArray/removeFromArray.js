cecace9185dc85e3eabd7f38169d53337f5eccfd
giomaldonado <giomaldonado@proton.me>
2026-09-07 16:55:46 -0600

    finished fundamentals 08

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...args) {
+  for (let arrIndex = arr.length - 1; arrIndex >= 0; arrIndex--) {
+
+    for (let argsIndex = args.length - 1; argsIndex >= 0; argsIndex--) {
+
+      if (arr[arrIndex] === args[argsIndex]) {
+        arr.splice(arrIndex, 1);
+        break;
+      }
+    }
+  }
+  return arr;
+
 };
 
 // Do not edit below this line
