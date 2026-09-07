29604312ca40d1e41b1adfdd7964983f67585063
giomaldonado <giomaldonado@proton.me>
2026-09-07 16:00:55 -0600

    finished foundations 06

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (word, times) {
+  if (times < 0) {
+    return "ERROR";
+  }
+  let retWord = "";
+  for (; times > 0; times--) {
+    retWord += word;
+  }
+  return retWord;
 };
 
 // Do not edit below this line
