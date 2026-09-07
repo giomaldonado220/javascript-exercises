44a4cfe3d7e8e1348f27b459bff9f60cbecf9582
giomaldonado <giomaldonado@proton.me>
2026-09-07 16:06:13 -0600

    finished foundations 07

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function (toReverse) {
+  let reversed = ""
+  for (let x = toReverse.length - 1; x >= 0; x--) {
+    reversed += toReverse.charAt(x);
+  }
+  return reversed;
 };
 
 // Do not edit below this line
