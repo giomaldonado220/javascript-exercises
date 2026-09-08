f907fd612dae6c7d4f2a51abc7d4266f5b2790ed
giomaldonado <giomaldonado@proton.me>
2026-09-08 10:01:06 -0600

    finished foundations 11

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (num) {
+  num = (num - 32) / 1.8;
+  return Math.round(num * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+
+const convertToFahrenheit = function (num) {
+  num = (num * 1.8) + 32;
+  return Math.round(num * 10) / 10;
 };
 
 // Do not edit below this line
