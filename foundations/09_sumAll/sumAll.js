99207c3eda865b8bd2d6c967045779ba5a88353d
giomaldonado <giomaldonado@proton.me>
2026-09-08 09:34:05 -0600

    finished foundations 09

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function (floor, ceil) {
 
+  if (!Number.isInteger(floor) || !Number.isInteger(ceil)) {
+    return "ERROR";
+  }
+  if (floor < 0 || ceil < 0) {
+    return "ERROR";
+  }
+  if (floor > ceil) {
+    let temp = ceil;
+    ceil = floor;
+    floor = temp;
+  }
+
+  let sumation = 0;
+  for (; ceil >= floor; ceil--) {
+    sumation += ceil;
+  }
+  return sumation;
 };
 
 // Do not edit below this line
